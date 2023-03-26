3e709d2dc32529444cdb7025088fd110213d175c
Robson Araujo <roobson.araujo02@gmail.com>
2023-03-26 10:15:59 -0300

    adicionar()

--- src/app/tarefa/tarefa.component.ts
+++ src/app/tarefa/tarefa.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tarefa',
   templateUrl: './tarefa.component.html',
   styleUrls: ['./tarefa.component.css']
 })
-export class TarefaComponent {
+export class TarefaComponent implements OnInit {
+
+  listaTarefas : any[]  = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.listaTarefas = [
+      {id: 0, nome:'Lavar o carro', concluida: false},
+      {id: 1, nome:'Ir ao mercado', concluida: true},
+      {id: 2, nome:'Lavar roupas', concluida: false}
+    ]
+  }
+
+  adicionar(nomeTarefa: string){
+    this.listaTarefas.push({id: this.listaTarefas.length, nome: nomeTarefa, concluida: false})
+  }
 
 }
